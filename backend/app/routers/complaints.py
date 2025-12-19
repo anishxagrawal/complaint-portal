e231f8aaeed5087f2713bf90ac36bcc839bb3575
anishxagrawal <agrawalanish052@gmail.com>
2025-12-19 22:46:30 +0000

    debugged the missing user_id problem

--- backend/app/routers/complaints.py
+++ backend/app/routers/complaints.py
@@ -12,10 +12,10 @@ router = APIRouter(prefix="/complaints", tags=["Complaint"])
 def post_complaints(user: ComplaintCreate, db: Session = Depends(get_db)):
 
     new_complaint = Complaint(
-        user_id=Complaint.user_id,
-        issue_type_id=Complaint.issue_type_id,
-        description=Complaint.description,
-        address=Complaint.address
+        user_id=user.user_id,
+        issue_type_id=user.issue_type_id,
+        description=user.description,
+        address=user.address
     )
 
     db.add(new_complaint)
