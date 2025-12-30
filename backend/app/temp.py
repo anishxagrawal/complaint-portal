ff62135d8fb83fde71c9d48f7dae4dcc8fa8bc7c
anishxagrawal <agrawalanish052@gmail.com>
2025-12-30 15:46:24 +0000

    Removed unnecessary files

--- backend/app/temp.py
+++
@@ -1,6 +0,0 @@
-from app.database import engine, Base
-from app.models import users, departments, issue_types, complaints
-
-Base.metadata.create_all(bind=engine)
-
-print("✅ Tables created successfully")
